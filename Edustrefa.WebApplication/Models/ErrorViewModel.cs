7dcc1c54cf49a7b1ab93c1a1eaac0875c4e32c14
Veroth <kamilwozniak1993@gmail.com>
2023-12-24 13:14:31 +0100

    initial commit

---
+++ Edustrefa.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Edustrefa.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
