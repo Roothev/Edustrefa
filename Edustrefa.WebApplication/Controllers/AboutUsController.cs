8e058339dcc06e55ece386a04a750b273242f76a
Veroth <kamilwozniak1993@gmail.com>
2023-12-26 16:23:03 +0100

    change style

---
+++ Edustrefa.WebApplication/Controllers/AboutUsController.cs
@@ -0,0 +1,26 @@
+﻿using Edustrefa.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Edustrefa.WebApplication.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        private readonly ILogger<AboutUsController> _logger;
+
+        public AboutUsController(ILogger<AboutUsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
